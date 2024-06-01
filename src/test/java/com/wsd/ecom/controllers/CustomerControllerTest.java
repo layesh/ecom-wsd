ee77559122dde360ee6b046d4a3d030fc659ce18
Layesh <layesh.iut@gmail.com>
2024-06-01 20:45:59 +0600

    Added test case for customer wishlist api.

---
+++ src/test/java/com/wsd/ecom/controllers/CustomerControllerTest.java
@@ -0,0 +1,27 @@
+package com.wsd.ecom.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+public class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void returnCustomerWishlist() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("rest-api/customer/1/wishlist").contentType(
+                MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.
+                jsonPath("$", Matchers.hasSize(1)));
+    }
+}
