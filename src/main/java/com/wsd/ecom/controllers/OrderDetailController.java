34998738acb1cda6607ca7d0fb84d6314b68d436
Layesh <layesh.iut@gmail.com>
2024-06-03 03:17:29 +0600

    Plugged order detail service in the controller to pull up original data.

--- src/main/java/com/wsd/ecom/controllers/OrderDetailController.java
+++ src/main/java/com/wsd/ecom/controllers/OrderDetailController.java
@@ -1,14 +1,29 @@
 package com.wsd.ecom.controllers;
 
+import com.wsd.ecom.services.OrderDetailService;
+import com.wsd.ecom.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
 @RestController
 @RequestMapping("/api/order-detail")
 public class OrderDetailController {
+    @Autowired
+    OrderDetailService orderDetailService;
+
     @GetMapping("/total-sale-amount-today")
     public String totalSaleOfToday() {
-        return "{total:0.0}";
+        Instant now = Instant.now();
+        Instant fromDate = now.atZone(ZoneId.of("UTC")).toInstant().truncatedTo(ChronoUnit.DAYS);
+        Instant toDate = fromDate.plus(1, ChronoUnit.DAYS).minusSeconds(1);
+
+        Double totalSaleToday = orderDetailService.getTotalSaleInDateRange(fromDate, toDate);
+        return "{total:" + totalSaleToday.toString() + "}";
     }
 }
--- src/test/java/com/wsd/ecom/controllers/OrderDetailControllerTest.java
+++ src/test/java/com/wsd/ecom/controllers/OrderDetailControllerTest.java
@@ -1,15 +1,21 @@
 package com.wsd.ecom.controllers;
 
+import com.wsd.ecom.services.OrderDetailService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.Instant;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(OrderDetailController.class)
@@ -18,12 +24,18 @@ public class OrderDetailControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private OrderDetailService orderDetailService;
+
     @Test
     public void shouldReturnTotalSaleAmountOfToday() throws Exception {
+        Instant now = Instant.now();
+        double totalSaleToday = 62000.0;
+        when(orderDetailService.getTotalSaleInDateRange(any(), any())).thenReturn(totalSaleToday);
+
         ResultActions response = mockMvc.perform(get("/api/order-detail/total-sale-amount-today")
                 .contentType(MediaType.APPLICATION_JSON));
-
         response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.greaterThanOrEqualTo(0.0)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.is(totalSaleToday)));
     }
 }
