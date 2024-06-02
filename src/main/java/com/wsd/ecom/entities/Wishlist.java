ea216db5bf1ec7251c4caf9d9acc0bf1a93b1048
Layesh <layesh.iut@gmail.com>
2024-06-02 11:52:06 +0600

    Added Wishlist entity.

---
+++ src/main/java/com/wsd/ecom/entities/Wishlist.java
@@ -0,0 +1,40 @@
+package com.wsd.ecom.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "t_wishlist")
+public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Wishlist(Customer customer, Product product) {
+        this.customer = customer;
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
