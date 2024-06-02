de1aa4c82d8968ab53d05474289fe7cefc887636
Layesh <layesh.iut@gmail.com>
2024-06-02 15:46:44 +0600

    Added test case for total sale of current day api.

---
+++ src/test/java/com/wsd/ecom/controllers/OrderDetailTest.java
@@ -0,0 +1,29 @@
+package com.wsd.ecom.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class OrderDetailTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnTotalSaleAmountOfToday() throws Exception {
+        ResultActions response = mockMvc.perform(get("/api/order-detail/total-sale-amount-today")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total", Matchers.greaterThanOrEqualTo(0.0)));
+    }
+}
